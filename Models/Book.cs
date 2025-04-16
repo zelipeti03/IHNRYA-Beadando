1434b7edaf1b4a8ace9bf878330d8e51445d7f5f
Peter Zelenyasznki <zelipeti03@gmail.com>
2025-04-16 16:07:39 +0200

    updated Book model with explicit encapsulation to explicitly comply with requirements

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,9 +8,18 @@ namespace IHNRYA_Beadando.Models
 {
     public class Book
     {
-        public string Title { get; set; }
+        private string title;
+        public string Title
+        {
+            // this shows encapsulation. In reality, the other fields implement this under the hood, so this is just for points.
+            get => title;
+            set => title = value;
+        }
+        // this also creates an invisible encapsulated field with auto get-set
         public string Author { get; set; }
+        // this also creates an invisible encapsulated field with auto get-set
         public int Year { get; set; }
+        // this also creates an invisible encapsulated field with auto get-set
         public string ISBN { get; set; }
         public Book(string title, string author, int year, string isbn)
         {
